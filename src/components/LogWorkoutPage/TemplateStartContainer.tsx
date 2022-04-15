23fb7e6356ee98306d811416c8b145bf3d68cfe6
Jason Cheung <jasoncheung0429@gmail.com>
2022-04-14 22:39:30 -0400

    Cleaned

--- src/components/LogWorkoutPage/TemplateStartContainer.tsx
+++ src/components/LogWorkoutPage/TemplateStartContainer.tsx
@@ -1,40 +1,41 @@
-import { Stack, Typography } from '@mui/material';
-import { TemplateType } from '../../firebase/Template';
-import { UserType } from '../../firebase/User';
-import { useWindowSize } from '../../hooks';
-import { useThemeState } from '../../states/ThemeState';
-import { useUserState } from '../../states/UserState';
-import TemplateStart from './TemplateStart';
+import { Stack, Typography } from "@mui/material";
+import { TemplateType } from "../../firebase/Template";
+import { UserType } from "../../firebase/User";
+import { useWindowSize } from "../../hooks";
+import { useThemeState } from "../../states/ThemeState";
+import { useUserState } from "../../states/UserState";
+import TemplateStart from "./TemplateStart";
 
 function TemplateStartContainer() {
 	const [width] = useWindowSize();
 	const user = useUserState() as UserType;
 	const theme = useThemeState();
+
 	return (
 		<Stack
 			direction='row'
 			gap={2}
 			style={{
-				flexWrap: 'wrap',
-				width: '100%',
-				alignItems: `${width > 400 ? '' : 'center'}`,
-				justifyContent: `${width > 400 ? '' : 'center'}`
-			}}>
+				flexWrap: "wrap",
+				width: "100%",
+				alignItems: `${width > 400 ? "" : "center"}`,
+				justifyContent: `${width > 400 ? "" : "center"}`
+			}}
+		>
 			{/* Render Templates Section */}
 			{user.templates.length === 0 ? (
 				<Typography
 					style={{
 						color: theme.text,
 						transition: theme.transition
-					}}>
+					}}
+				>
 					You currently have no templates.
 				</Typography>
 			) : (
 				user.templates
 					.map((t) => t)
-					.sort((a: TemplateType, b: TemplateType) =>
-						a.name.localeCompare(b.name)
-					)
+					.sort((a: TemplateType, b: TemplateType) => a.name.localeCompare(b.name))
 					.map((template: TemplateType, idx: number) => (
 						<TemplateStart key={idx} template={template} />
 					))
